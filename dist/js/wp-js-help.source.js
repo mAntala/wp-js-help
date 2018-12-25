b61746e4b6c35ad9c0e215ecac582744f06794d1
Maroš Antala <maros.antala@gmail.com>
2018-12-25 17:09:13 +0100

    Nah man, forgot to run npm build

--- dist/js/wp-js-help.source.js
+++ dist/js/wp-js-help.source.js
@@ -10,15 +10,51 @@ var WPJSHelp = function WPJSHelp() {
     }
 
     return;
-  };
+  }; // PUBLIC FUNCTIONS
+
+  /**
+   * Check, if actual page is HOMEPAGE.
+   * @return {Boolean}
+   */
 
 
   var isHome = function isHome() {
     return hasBodyClass('home');
   };
+  /**
+   * Check, if actual page is CATEGORY / ARCHIVE.
+   * @return {Boolean}
+   */
+
+
+  var isCategory = function isCategory() {
+    return hasBodyClass('category') || hasBodyClass('archive');
+  };
+  /**
+   * Check, if actual page is ARTICLE.
+   * @return {Boolean} [description]
+   */
+
+
+  var isArticle = function isArticle() {
+    return hasBodyClass('single') || hasBodyClass('single-post');
+  };
+  /**
+   * Check, if actual page is SEARCH.
+   * I mean page with search result. Just make it clear.
+   * @return {Boolean}
+   */
+
+
+  var isSearch = function isSearch() {
+    return hasBodyClass('search') || hasBodyClass('search-results');
+  };
 
   return {
-    isHome: isHome
+    isHome: isHome,
+    isCategory: isCategory,
+    isArticle: isArticle,
+    isSearch: isSearch
   };
 };
 
